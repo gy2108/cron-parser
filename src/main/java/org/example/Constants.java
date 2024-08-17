5f4551637c1e94229a2dc3bb52f79b31cf166ba3
gy2108 <gopalyadav@Gopals-MacBook-Air.local>
2024-08-17 13:13:40 +0530

    constant class final and private

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -1,6 +1,9 @@
 package org.example;
 
-public class Constants {
+public final class Constants {
+    private Constants(){
+
+    }
     public static final int Field_Name_Column_Length = 14;
     public static final String Command = "command";
     public static final int CRON_ARGUMENTS_LEN =6;
